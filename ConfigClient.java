152766150f74ce5c6e75146e8414451394e7d8ed
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-16 10:40:13 +0530

    Add files via upload

--- ConfigClient.java
+++ ConfigClient.java
@@ -8,10 +8,26 @@ import com.pojo.Student;
 public class ConfigClient {
     public static void main(String[] args) {
         StudentDao studentDao = new StudentDao();
-        Student student = new Student("sai", "sri", "sai@gmail.com");
-        studentDao.saveStudent(student);
+     /*   Student student = new Student("minu", "sri", "minu@gmail.com");
+       studentDao.saveStudent(student);
+
+       student = new Student("sai", "sri", "sai@gmail.com");
+       studentDao.saveStudent(student);*/
 
         List < Student > students = studentDao.getStudents();
         students.forEach(s -> System.out.println(s));
+        
+        //update
+     /*   Student obj=new Student();
+        obj.setId(1);
+        obj.setFirstName("Sai sri");
+        studentDao.update(obj);
+        students = studentDao.getStudents();
+        students.forEach(s -> System.out.println(s));*/
+        //delete
+      studentDao.deleteStudent(1);
+        students = studentDao.getStudents();
+        students.forEach(s -> System.out.println(s));
+        
     }
 }

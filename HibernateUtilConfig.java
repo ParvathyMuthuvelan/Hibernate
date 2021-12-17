523995c3e4174995f4da5b46ede9bdbe41357fbe
ParvathyMuthuvelan <78962020+ParvathyMuthuvelan@users.noreply.github.com>
2021-12-17 10:36:50 +0530

    Add files via upload

--- HibernateUtilConfig.java
+++ HibernateUtilConfig.java
@@ -8,6 +8,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
+import com.onetomany.Department;
+import com.onetomany.Employee;
+import com.onetomany.Skill;
+import com.onetoone.Author;
+import com.onetoone.Book;
 import com.pojo.Student;
 
 public class HibernateUtilConfig {
@@ -21,22 +26,27 @@ public class HibernateUtilConfig {
 				// Hibernate settings equivalent to hibernate.cfg.xml's properties
 				Properties settings = new Properties();
 				settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/test?useSSL=false");
+				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/ormnew?useSSL=false");
 				settings.put(Environment.USER, "root");
 				settings.put(Environment.PASS, "root");
 				settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
 				settings.put(Environment.SHOW_SQL, "true");
 
-				settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+				//settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
-				settings.put(Environment.HBM2DDL_AUTO, "create");
+				settings.put(Environment.HBM2DDL_AUTO, "validate");
 
 				configuration.setProperties(settings);
 
-				configuration.addAnnotatedClass(Student.class);
-				
-				
+			//	configuration.addAnnotatedClass(Student.class);
+			
+		
+			//	configuration.addAnnotatedClass(Book.class);
+				//configuration.addAnnotatedClass(Author.class);
+				configuration.addAnnotatedClass(Employee.class);
+				configuration.addAnnotatedClass(Department.class);
+				configuration.addAnnotatedClass(Skill.class);
 				//The main purpose of a service registry is to hold, manage and provide access to services.
 				ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
 						.applySettings(configuration.getProperties()).build();
